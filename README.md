06dc08547e268e23ff48a1fe8f1623ad774d9f7f
saurav5101998 <sauravlpu11@gmail.com>
2024-06-09 12:22:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Saurav's Portfolio
+
+Welcome to my portfolio! I'm Saurav, a Software Engineer specializing in frontend development. This portfolio showcases my work, skills, and projects.
+
+## About Me
+
+I am a frontend developer with 3 years of experience, proficient in React.js, Redux, and React Native. I am passionate about creating seamless user experiences and transforming concepts into interactive applications.
+
+## Projects
+
+### Vistabee
+- Developed a multifunctional web app to streamline the sales process for various real estate projects.
+- Enhanced accessibility for employees, customers, and new users.
+- Led frontend development, creating diverse components and fixing over 100 bugs in a short timeframe.
+
+### Daftar
+- Developed an Android app allowing users to schedule cabs for any duration.
+- Created React Native components and configured Android projects.
+- Integrated Google Maps and ZegoCloud SDK for mapping and call features.
+
+### Sofa Website using Threekit
+- A 3D configurator to visualize how a sofa will fit and look in your living space.
+
+### Redcliffe Labs
+- Developed a comprehensive healthcare website with a focus on user-friendly interfaces and seamless interactions.
+
+## Skills
+
+- **Frontend Development**: HTML, CSS, JavaScript, React.js, Redux
+- **Mobile Development**: React Native
+- **Tools & Technologies**: Jest, Enzyme, Material UI, Git, Three.js
+
+## Testimonials
+
+> "Collaborating with Saurav was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Saurav's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Saurav is the ideal partner."
+- Michael Johnson, Director of AlphaStream Technologies
+
+> "Saurav's expertise in frontend development is unmatched. His ability to quickly address and fix bugs, combined with his creativity in crafting user-friendly interfaces, made him an invaluable asset to our team. Highly recommended for any web development projects."
+- Sarah Lee, Project Manager at Tech Solutions
+
+## Contact
+
+Feel free to reach out to me for collaborations or inquiries:
+
+- **Email**: [saurav@example.com](mailto:saurav@example.com)
+- **LinkedIn**: [linkedin.com/in/saurav](https://www.linkedin.com/in/saurav)
+- **GitHub**: [github.com/saurav](https://github.com/saurav)
+
+## Approach
+
+My approach to web development involves thorough planning and strategy, continuous development with progress updates, and final development and launch. I collaborate closely with clients to map out goals and key functionalities, ensuring a smooth and efficient development process.
+
+### Planning & Strategy
+We'll collaborate to map out your website's goals, target audience, and key functionalities. We'll discuss things like site structure, navigation, and content requirements.
+
+### Development & Progress Update
+Once we agree on the plan, I cue my lofi playlist and dive into coding. From initial sketches to polished code, I keep you updated every step of the way.
+
+### Development & Launch
+This is where the magic happens! Based on the approved design, I'll translate everything into functional code, building your website from the ground up.
---
+++ data/index.ts
@@ -0,0 +1,213 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title:
+      "Currently enhancing my skills with react-table and react-query for advanced data handling and optimization.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Role based Real Estate Project",
+    des: "Experience the future of real estate with our immersive property visualization",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.png"],
+    link: "/vistabee.com",
+  },
+  {
+    id: 2,
+    title: "Daftar Web & Mobile App for Easy Ride Schedule",
+    des: "Simplify your Ride experience with Daftar. Seamlessly commute between Home and Office.",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.png"],
+    link: "/gods-eye.com",
+  },
+  {
+    id: 3,
+    title: "Sofa Website using Threekit",
+    des: "A 3D configurator to visualize how a sofa will fit and look in your living space.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.png"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 5,
+    title: "Redcliffe Labs Healthcare Website",
+    des: "Developed an intuitive and user-friendly healthcare website for Redcliffe Labs, enhancing patient interaction and accessibility.",
+    img: "/p4.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.png"],
+    link: "/redcliffelabs.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Saurav has been a remarkable experience. His mastery of HTML, CSS, and JavaScript, combined with his expertise in React.js and Redux, enabled us to create a seamless and highly responsive web application. His attention to detail and problem-solving abilities are top-notch.",
+    name: "Saurabh Majumdar",
+    title: "SDE-II at Cleartrip (Flipkart)",
+    img: "/saurabh.png",
+  },
+  {
+    quote:
+      "Saurav's proficiency in React Native was crucial in the successful launch of our mobile application. His ability to quickly integrate complex features and ensure a smooth user experience was impressive. He's a reliable and skilled developer who consistently exceeds expectations.",
+    name: "Prabhat Singh",
+    title: "Founder at Daftar Route",
+    img: "/defaultProfile.webp",
+  },
+  {
+    quote:
+      "I had the pleasure of collaborating with Saurav on several projects. His knowledge of frontend technologies, particularly React.js and Redux, is exceptional. He brings a unique blend of creativity and technical skill to the table, making him an invaluable asset to any team.",
+    name: "Prashant Singh",
+    title: "Co-Founder at Digitize Solution",
+    img: "/prashant.png",
+  },
+  {
+    quote:
+      "Saurav's dedication to his work and his ability to quickly adapt to new challenges are commendable. His skills in HTML, CSS, and JavaScript, along with his expertise in React.js, allowed us to deliver high-quality products on time. He's a fantastic team player and a great collaborator.",
+    name: "Vikash",
+    title: "Project Manager at Redcliffe Labs ( LIMS )",
+    img: "/defaultProfile.webp",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "DhobiLite",
+    img: "/dhobilite.jpeg",
+    nameImg: "/dhobilite.jpeg",
+  },
+  {
+    id: 2,
+    name: "Redcliffe",
+    img: "/redcliffe.avif",
+    nameImg: "/redcliffe.avif",
+  },
+  {
+    id: 3,
+    name: "Digitize",
+    img: "/digitize.png",
+    nameImg: "/digitize.png",
+  },
+  {
+    id: 4,
+    name: "MindNerves",
+    img: "/MNT.jpeg",
+    nameImg: "/MNT.jpeg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Business Development Executive",
+    desc: "Facilitated the growth of business opportunities through strategic planning and market analysis.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 2,
+    title: "FrontEnd Developer",
+    desc: "Developed and optimized web applications using React.js, enhancing user experience and performance.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 3,
+    title: "React Native Developer",
+    desc: "Led the development of mobile apps for Android, from initial concept to deployment.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Software Engineer",
+    desc: "Implemented and maintained user-facing features with modern frontend technologies, ensuring high performance and responsiveness.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    ulr: "https://github.com/saurav5101998",
+  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  //   url: "",
+  // },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/sauravkumar5101998/",
+  },
+];
